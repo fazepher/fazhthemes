f9f5b0fab041d9764edd9510c2e793d44e0d5154
Fernando A Zepeda H <fazepher@gmail.com>
2020-08-17 00:56:37 -0500

    Importar ggplot2 al NAMESPACE.

---
+++ R/fazhthemes.R
@@ -0,0 +1,17 @@
+#' fazhthemes: A package with {ggplot2} themes.
+#'
+#' The {fazhthemes} package provides the author's (Fernando Antonio Zepeda Herrera) personal
+#' themes for plotting in R.
+#'
+#' @section Lucified themes:
+#' The lucify_theme_* functions are modified versions of {ggplot2}'s core themes.
+#'
+#' @docType package
+#' @name fazhthemes
+NULL
+#> NULL
+
+### Paquetes a importar ###
+
+#' @import ggplot2
+NULL
--- R/lucify_themes.R
+++ R/lucify_themes.R
@@ -1,7 +1,8 @@
 
 #' Lucified ggplot2 themes
 #'
-#' These functions "lucify" a core ggplot2 theme.
+#' These functions "lucify" (inside joke for modifying) a core {ggplot2} theme,
+#' as described in (\code{\link[ggplot2]{ggtheme}}).
 #' The basics of lucified themes are Century Gothic fonts, bigger sizes of text elements and margins,
 #' as well as slightly different colors and centered title and subtitles.
 #'
@@ -14,36 +15,36 @@
 #'
 lucify_basics <- function(text_family = "Century Gothic", text_size = 15, text_color = "#3C3C3C"){
 
-  ggplot2::theme(line = ggplot2::element_line(colour = "#666666"),
-                 rect = ggplot2::element_rect(colour = "#C6C6C6"),
-                 text = ggplot2::element_text(family = text_family,
-                                              colour = text_color,
-                                              size = text_size),
-                 axis.title.x = ggplot2::element_text(size = ggplot2::rel(1.2),
-                                                      margin = ggplot2::margin(t = 15)),
-                 axis.title.x.top = ggplot2::element_text(size = ggplot2::rel(1.2),
-                                                          margin = ggplot2::margin(b = 15)),
-                 axis.title.y = ggplot2::element_text(size = ggplot2::rel(1.2),
-                                                      margin = ggplot2::margin(r = 30)),
-                 axis.title.y.right = ggplot2::element_text(size = ggplot2::rel(1.2),
-                                                            margin = ggplot2::margin(l = 30)),
-                 axis.text.x = ggplot2::element_text(size = ggplot2::rel(0.825),
-                                                     margin = ggplot2::margin(t = 8)),
-                 axis.text.x.top = ggplot2::element_text(size = ggplot2::rel(0.825),
-                                                         margin = ggplot2::margin(b = 8)),
-                 axis.text.y = ggplot2::element_text(margin = ggplot2::margin(r = 8)),
-                 axis.text.y.right = ggplot2::element_text(margin = ggplot2::margin(l = 8)),
-                 axis.ticks = ggplot2::element_blank(),
-                 legend.margin = ggplot2::margin(t = 15, r = 15, b = 15, l = 15),
-                 legend.title = ggplot2::element_text(size = ggplot2::rel(1.15)),
-                 panel.spacing = ggplot2::unit(15,"pt"),
-                 plot.title = ggplot2::element_text(size = ggplot2::rel(1.3),
-                                                    hjust = 0.5,
-                                                    margin = ggplot2::margin(b = 20)),
-                 plot.subtitle = ggplot2::element_text(hjust = 0.5,
-                                                       margin = ggplot2::margin(t = -12.5, b = 20)),
-                 plot.margin = ggplot2::margin(t = 10, r = 10, b = 10, l = 10),
-                 strip.text = ggplot2::element_text(size = ggplot2::rel(1.1),
-                                                    margin = ggplot2::margin(t = 8, r = 8, b = 8, l = 8)))
+  theme(line = element_line(colour = "#666666"),
+        rect = element_rect(colour = "#C6C6C6"),
+        text = element_text(family = text_family,
+                            colour = text_color,
+                            size = text_size),
+        axis.title.x = element_text(size = rel(1.2),
+                                    margin = margin(t = 15)),
+        axis.title.x.top = element_text(size = rel(1.2),
+                                        margin = margin(b = 15)),
+        axis.title.y = element_text(size = rel(1.2),
+                                    margin = margin(r = 30)),
+        axis.title.y.right = element_text(size = rel(1.2),
+                                          margin = margin(l = 30)),
+        axis.text.x = element_text(size = rel(0.825),
+                                   margin = margin(t = 8)),
+        axis.text.x.top = element_text(size = rel(0.825),
+                                       margin = margin(b = 8)),
+        axis.text.y = element_text(margin = margin(r = 8)),
+        axis.text.y.right = element_text(margin = margin(l = 8)),
+        axis.ticks = element_blank(),
+        legend.margin = margin(t = 15, r = 15, b = 15, l = 15),
+        legend.title = element_text(size = rel(1.15)),
+        panel.spacing = unit(15,"pt"),
+        plot.title = element_text(size = rel(1.3),
+                                  hjust = 0.5,
+                                  margin = margin(b = 20)),
+        plot.subtitle = element_text(hjust = 0.5,
+                                     margin = margin(t = -12.5, b = 20)),
+        plot.margin = margin(t = 10, r = 10, b = 10, l = 10),
+        strip.text = element_text(size = rel(1.1),
+                                  margin = margin(t = 8, r = 8, b = 8, l = 8)))
 
 }
